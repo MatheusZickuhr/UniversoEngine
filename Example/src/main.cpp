c19b9150381368fa412b16542077dff1df6dc9c6
MatheusZickuhr <mz97callofduty542@gmail.com>
2021-02-14 20:20:44 -0300

    remove some not used includes from Engine.h, simplified Example project

--- Example/src/main.cpp
+++ Example/src/main.cpp
@@ -1,7 +1,58 @@
 
 #include "Engine.h"
-#include "ExampleLevel.h"
+#include "DebugCameraController.h"
 
+class Crate : public engine::GameObject {
+
+public:
+
+	using engine::GameObject::GameObject;
+
+	void onStart() override {
+
+	}
+
+	void onUpdate(float deltaTime) override {
+
+	}
+};
+
+class ExampleLevel : public engine::Level {
+
+private:
+	std::unique_ptr<engine::DebugCameraController> cameraInput;
+
+	std::shared_ptr<Crate> crate;
+	std::shared_ptr<Crate> crateClone;
+
+public:
+
+	void onStart() override {
+		this->cameraInput = std::make_unique<engine::DebugCameraController>(this->camera);
+
+		auto mesh = std::make_shared<engine::Mesh>("res/models/crate/crate.obj");
+		auto texture = std::make_shared<engine::Texture>("res/textures/crate/crate.jpg");
+
+		crate = std::make_shared<Crate>(mesh, texture);
+		crate->transform->rotationAxis = { 1.0f, 0.0f, 0.0f };
+
+		crateClone = std::make_shared<Crate>(mesh, texture);
+		crateClone->transform->position = { 3.0f, 0.0f, 0.0f };
+		crateClone->transform->rotationAxis = { 1.0f, 0.0f, 0.0f };
+
+		this->appendGameObject(crate);
+		this->appendGameObject(crateClone);
+	}
+
+	void onUpdate(float deltaTime) override {
+		cameraInput->update(deltaTime);
+
+		constexpr float speed = 25.0f;
+
+		crate->transform->rotationAngle += deltaTime * speed;
+		crateClone->transform->rotationAngle -= deltaTime * speed;
+	}
+};
 
 int main() {
 	engine::Engine engine(800, 600, "Universo Engine");
--- UniversoEngine/src/Engine.h
+++ UniversoEngine/src/Engine.h
@@ -1,39 +1,13 @@
 #pragma once
 
-// c++ std
-#include <stdio.h>
-#include <iostream>
 #include <memory>
-#include <map>
-#include <array>
-#include <fstream>
-#include <string>
-#include <vector>
 
-// third party libraries
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
-// engine
-#include "renderer/renderer_api/VertexArray.h"
-#include "renderer/renderer_api/VertexBuffer.h"
-#include "renderer/renderer_api/IndexBuffer.h"
-#include "renderer/renderer_api/Shader.h"
-#include "renderer/renderer_api/ShaderProgram.h"
-#include "renderer/renderer_api/Texture.h"
-#include "renderer/renderer_api/Drawer.h"
-#include "renderer/Vertex.h"
-#include "renderer/Mesh.h"
-#include "renderer/Camera.h"
 #include "renderer/Renderer3D.h"
-#include "Transform.h"
-#include "GameObject.h"
 #include "Input.h"
-#include "Level.h"
 #include "LevelLoadingManager.h"
-
 
 namespace engine {
 
